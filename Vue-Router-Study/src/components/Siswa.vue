346ba58d4467711cbbad37ab08dc62c0819f2709
ahmadabdulghani <47271805+ahmadabdulghani@users.noreply.github.com>
2019-03-18 16:12:56 +0700

    Add files via upload

---
+++ Vue-Router-Study/src/components/Siswa.vue
@@ -0,0 +1,29 @@
+<template>
+ <div><br>
+  <center>
+  <table border="1">
+    <tr>
+      <td><center><b>Kelas</b></center></td>
+      <td><center><b>Nama</b></center></td>
+    </tr>
+    <tr>
+      <td>XII - RPL 1</td>
+      <td>Ahmad Abdul Ghani</td>
+    </tr>
+    <tr>
+      <td>XII - RPL 2</td>
+      <td>Alfian Simajuntak</td>
+    </tr>
+    <tr>
+      <td>XII - RPL 3</td>
+      <td>Deo William</td>
+    </tr>
+  </table>
+  </center>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
